9c4134ee7fbf0f4c5acf833e78b08dca9000a098
Juan Lessa <61157086+juanlessa@users.noreply.github.com>
2020-05-19 13:32:16 +0100

    alterações pendentes

--- SQL/SQL_Indexes.sql
+++ SQL/SQL_Indexes.sql
@@ -10,3 +10,5 @@ CREATE INDEX i_Utilizador_nome ON SPORT_SHOP.[Utilizador](nome);
 CREATE INDEX i_Produto_tipoID_nome ON SPORT_SHOP.[Produto](tipo_ID, nome);
 CREATE INDEX i_Loja_lojacode ON SPORT_SHOP.[Loja](loja_code);
 CREATE INDEX i_Venda_dataVenda_IDvenda ON SPORT_SHOP.[Venda](data_venda, venda_ID);
+
+                                                      
