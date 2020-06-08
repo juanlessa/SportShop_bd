4c94596429cb0f3c7ede863ebe29bbc8ef3888da
Juan Lessa <61157086+juanlessa@users.noreply.github.com>
2020-06-08 13:03:58 +0100

    Update SQL_Indexes.sql

--- SQL/SQL_Indexes.sql
+++ SQL/SQL_Indexes.sql
@@ -1,14 +1,10 @@
 USE SPORT_SHOP
---doubts:
--- como ordenar a data por ordem contrária
+GO
 
 -- ===============
 -- === INDEXES ===
 -- ===============
 
-CREATE INDEX i_Utilizador_nome ON SPORT_SHOP.[Utilizador](nome);
-CREATE INDEX i_Produto_tipoID_nome ON SPORT_SHOP.[Produto](tipo_ID, nome);
-CREATE INDEX i_Loja_lojacode ON SPORT_SHOP.[Loja](loja_code);
-CREATE INDEX i_Venda_dataVenda_IDvenda ON SPORT_SHOP.[Venda](data_venda, venda_ID);
+CREATE INDEX i_produto_nome ON SPORT_SHOP.[Produto](tipo_ID, nome_Produto, marca, especificidade, loja_code)
 
                                                       
